e5c43ec9ac1353f1d5d5c378b15726b1d54f489d
Aether <64797587+Aeastr@users.noreply.github.com>
2024-07-20 19:53:09 -0700

    Initial Commit

---
+++ VisionHome/VisionHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionHomeApp.swift
+//  VisionHome
+//
+//  Created by Aether on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct VisionHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionHomeTests/VisionHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionHomeTests.swift
+//  VisionHomeTests
+//
+//  Created by Aether on 20/07/2024.
+//
+
+import Testing
+@testable import VisionHome
+
+struct VisionHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
